2e8892972f95cf672aef61434b745839d7820cb0
Rebecca Eilering <rebeccaeilering@gmail.com>
2019-05-03 22:35:30 -0500

    edit about page, all font size to h1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,9 +5,18 @@ class About extends Component {
   render() {
     return (
       <div className="about">
-        <h1>
-          Edit About component or pages/about.jsx to include your information.
-        </h1>
+        <h1>About Me</h1>
+        <p>I'm a web developer specializing in HTML, CSS, JavaScript/jQuery, building themes on the .Net framework, and taking on new challenges. I have a background in design and art. I graduated with an MFA from Ohio University in 2005 but have transitioned from working in the art field to working as a web developer.</p>
+        <p>I'm is originally from the St. Louis area but recently moved to Springfield, IL. I lived in Athens, OH while studying at Ohio University.
+        </p>
+        <p>Outside of being a web developer, I like to draw, read (nonfiction, comics, graphic novels), watch YouTube videos, listen to podcasts, hike, play Pokemon Go, ride my bike, build with LEGO, go on roadtrips and travel, thrifting and antiquing. I like playing with my tuxedo cat named Map. I also love steak, Mexican food and pizza. My favorite dessert is frozen custard. I also like listening to music - mostly indie, alternative, and punk from the 1980s and 1990s.
+        </p>
+        <p>I have a long-term partner named Ben. He's my rock and is overall the bestest person.</p>
+        <p>Visit <a href="https://www.rebeccaeilering.me">my website</a> to learn even more about me!
+        </p>
+
+
+
       </div>
     );
   }
