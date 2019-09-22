93a32487e8d13a85afe6f77fee18565bc45e88b9
Rebecca Eilering <rebeccaeilering@gmail.com>
2019-09-22 17:43:05 -0500

    added tags and categories to home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { graphql } from "gatsby";
+import { Link, graphql } from "gatsby";
+import _ from "lodash";
 import Layout from "../layout";
 import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
@@ -9,14 +10,41 @@ import config from "../../data/SiteConfig";
 class Index extends React.Component {
   render() {
     const postEdges = this.props.data.allMarkdownRemark.edges;
+    const groupTags = this.props.data.allMarkdownRemark.tags;
+    const groupCategory = this.props.data.allMarkdownRemark.category;
+
     return (
       <Layout>
-        <div className="main-container">
+        <main className="main-container home">
           <Helmet title={config.siteTitle} />
           <SEO />
-          <h1>Latest Posts</h1>
-          <PostListing postEdges={postEdges} />
-        </div>
+          <section className="posts">
+            <h1>Latest Posts</h1>
+            <PostListing postEdges={postEdges} />
+          </section>
+          <aside>
+            <span>Categories</span>
+            <ul className="tags-cat">
+              {groupCategory.map(category => (
+                <li key={category.fieldValue}>
+                  <Link to={`/categories/${_.kebabCase(category.fieldValue)}/`}>
+                    {category.fieldValue} ({category.totalCount})
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <span>Tags</span>
+            <ul className="tags-cat">
+              {groupTags.map(tag => (
+                <li key={tag.fieldValue}>
+                  <Link to={`/tags/${_.kebabCase(tag.fieldValue)}/`}>
+                    {tag.fieldValue} ({tag.totalCount})
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </aside>
+        </main>
       </Layout>
     );
   }
@@ -26,11 +54,19 @@ export default Index;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
-  query IndexQuery {
+    query IndexQuery {
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [fields___date], order: DESC }
     ) {
+      category: group(field: frontmatter___category) {
+        fieldValue
+        totalCount
+      }
+      tags: group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
       edges {
         node {
           fields {
