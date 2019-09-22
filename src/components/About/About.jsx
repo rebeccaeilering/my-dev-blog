8b5b16af6d399e0c74158cfbd4e6fe6231df4beb
Rebecca Eilering <rebeccaeilering@gmail.com>
2019-09-22 17:57:35 -0500

    updated my bio - fixing typos, grammar etc

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,12 +6,11 @@ class About extends Component {
     return (
       <div className="about">
         <h1>About Rebecca</h1>
-        <p>I'm a web developer specializing in HTML, CSS, JavaScript/jQuery, building themes on the .Net framework, and taking on new challenges. I have a background in design and art. I graduated with an MFA from Ohio University in 2005 but have transitioned from working in the art field to working as a web developer.</p>
-        <p>I'm is originally from the St. Louis area but recently moved to Springfield, IL. I lived in Athens, OH while studying at Ohio University.
+        <p>I'm a web developer based in Springfield, IL (STL native) specializing in HTML, CSS, JavaScript, jQuery, building themes on the .Net framework, and taking on new challenges.</p>
+        <p>I have a background in design and art. I graduated with an MFA from Ohio University in 2005. I started transitioning from working in the art world to working as a web developer in 2011.</p>
+        <p>Outside of being a web developer, I like to make art (drawing mostly), read (nonfiction, comics, graphic novels), watch YouTube videos, listen to podcasts, hike, play Pokemon Go, ride my bike, build with LEGO, go on roadtrips and travel, shop at thrift and antique stores, take photos of architecture, roadside attractions, old signs, americana. I like playing with my super awesome/vicious tuxedo cat named Map. I also love steak, Mexican food and pizza. My favorite dessert is frozen custard from Bobby's in Maryville, IL. I also like listening to music - mostly indie, alternative, new wave and punk from the 1980s and 1990s.
         </p>
-        <p>Outside of being a web developer, I like to draw, read (nonfiction, comics, graphic novels), watch YouTube videos, listen to podcasts, hike, play Pokemon Go, ride my bike, build with LEGO, go on roadtrips and travel, thrifting and antiquing. I like playing with my tuxedo cat named Map. I also love steak, Mexican food and pizza. My favorite dessert is frozen custard. I also like listening to music - mostly indie, alternative, and punk from the 1980s and 1990s.
-        </p>
-        <p>I have a long-term partner named Ben. He's my rock and is overall the bestest person.</p>
+        <p>I have a long-term partner named Ben. He's my rock and is overall the best person in the whole world.</p>
         <p>Visit <a href="https://www.rebeccaeilering.me">my website</a> to learn even more about me!
         </p>
 
