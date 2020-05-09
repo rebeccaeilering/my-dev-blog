cb3ae144315eb739835495fc7c869312fff3658d
Rebecca Eilering <rebeccaeilering@gmail.com>
2020-05-08 21:13:03 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-18a99344229b4e39f88e.js"
+    "url": "webpack-runtime-421dcd3386cbea229d11.js"
   },
   {
     "url": "app-470530b71a1bc2982fe3.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "549925538613cd99d9c82d070ed129c7"
+    "revision": "95654fa3effb26b7300870d436671551"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
